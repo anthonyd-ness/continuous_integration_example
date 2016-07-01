d90b59b79ed5f6b209f63b144d68f0a512f3fce5
PriyeshGaikwad <priyesh.gaikwad@gmail.com>
2016-07-01 15:27:47 +0530

    deploy target change

--- software/deployment/config/deploy.rb
+++ software/deployment/config/deploy.rb
@@ -4,7 +4,7 @@ load '/usr/share/tomcat6/scripts/config/aws.config'
 
 sdb = AWS::SimpleDB.new
 
-set :sdb_domain, "cd00"
+set :sdb_domain, "target"
 
 set :domain do
   item = sdb.domains["#{sdb_domain}"].items['parameters']
